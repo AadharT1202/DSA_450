0a4fd3f3db3e5819cbeb58e8dec4fc3d94f75fac
AadharT1202 <aadhar.toshniwal12@gmail.com>
2022-02-03 13:34:56 +0530

    Missing Element in AP

---
+++ 4.SearchingAndSorting/27.FindTheMissingElementInAP/solution.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int findMissingUtil(int arr[], int low, int high, int diff)
+{   
+    int mid;
+    while (low <= high)
+    {   
+        mid = (low + high) / 2;
+        cout<<"High: "<<high<<" "<<"Low: "<<low<<" "<<"Mid: "<<mid<<endl;
+        if ((arr[mid] - arr[0]) / diff == mid)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return arr[high] + diff;
+}
+
+int findMissing(int arr[], int n)
+{
+    int diff = (arr[n - 1] - arr[0]) / n;
+    return findMissingUtil(arr, 0, n - 1, diff);
+}
+ 
+int main()
+{
+    int arr[] = {2, 4, 8, 10, 12, 14};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "The missing element is "
+         << findMissing(arr, n)<<endl;
+    return 0;
+}
