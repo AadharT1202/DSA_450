8c1b76143c8a3b198b8748b29f5a805859361b2f
AadharT1202 <aadhar.toshniwal12@gmail.com>
2022-02-22 19:12:13 +0530

    Division using bitwise operators

--- 14.BitManipulation/8.Divide/solution.cpp
+++ 14.BitManipulation/8.Divide/solution.cpp
@@ -1,1 +1,42 @@
 #include<iostream>
+using namespace std;
+
+int division(long long divisor, long long divident)
+{
+    int sign = 1;
+    if(divisor<0)
+        sign *= -1;
+    if(divident<0)
+        sign *= -1;
+            
+    long long n,m;
+    n = abs(divident);
+    m = abs(divisor);
+    long long q = 0;
+    long long t = 0;
+
+    for(int i=31; i>=0; i--)
+    {
+        if(t+(m<<i) <= n)
+        {
+            t += (m<<i);
+            q += (1<<i);
+        }
+    }
+
+    if(sign==1)
+        return q;
+    else
+        return -1*q;
+}
+
+int main()
+{
+    long long divisor, divident;
+    cout<<"Enter the divident: ";
+    cin>>divident;
+    cout<<"Enter the divisor: ";
+    cin>>divisor;
+    cout<<"Quotient is: "<<division(divisor,divident)<<endl;
+
+}
