51a8fea6c31a86a8e5a6d599096b6de6ea31fbb0
AadharT1202 <aadhar.toshniwal12@gmail.com>
2022-02-24 23:11:16 +0530

    Word Break Problem

--- 13.Trie/3.WordBreakProblem/solution.cpp
+++ 13.Trie/3.WordBreakProblem/solution.cpp
@@ -1,38 +1,75 @@
 #include<iostream>
 using namespace std;
+const int Alphabet_Size = 26;
 
 struct TrieNode{
-    int pc;
-    struct TrieNode *children[26];
+    struct TrieNode *children[Alphabet_Size];
+    bool iswordEnd;
 };
 
+//Returns a new TrieNode initialized to Null.
 struct TrieNode* getNode()
 {
-    struct TrieNode* temp = new TrieNode;
-    temp->pc = 1;
-    for(int i=0; i<26; i++)
-        temp->children[i]=NULL;
+    struct TrieNode *temp =  new TrieNode;
+    temp->iswordEnd = false;
+    for(int i=0; i<Alphabet_Size; i++)
+    {
+        temp->children[i] = NULL;
+    }
     return temp;
 }
 
-void insert(struct TrieNode* root, string key)
+void insert(struct TrieNode *root, string key)
 {
-    struct TrieNode* temp = root;
+    struct TrieNode *temp = root;
     for(int i=0; i<key.length(); i++)
     {
         int idx = key[i] - 'a';
         if(temp->children[idx]==NULL)
             temp->children[idx] = getNode();
-        else
-            temp->children[idx]->pc++;
         temp = temp->children[idx];
     }
+    temp->iswordEnd = true;
+}
+
+bool check(struct TrieNode *root, string inp)
+{
+    struct TrieNode* temp = root;
+    if(inp.size()==0)
+        return true;
+
+    for(int i=0; i<inp.length(); i++)
+    {
+        int idx = inp[i]-'a';
+        if(temp->children[idx]==NULL)
+        {
+            return false;
+        }
+        
+        else if(temp->children[idx]->iswordEnd == true){
+                temp = root;
+        }
+
+        else
+            temp = temp->children[idx];
+
+    }
+    return true;
 }
 
 int main()
 {
-    string arr[] = { "i", "like", "sam", "sung", "samsung", "mobile", "ice", 
-  "cream", "icecream", "man", "go", "mango"};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    cout<<n;
+    string arr[] = {"mobile","samsung","sam",
+                           "sung","ma\n","mango",
+                           "icecream","and","go","i",
+                           "like","ice","cream"};
+    int n = sizeof(arr)/sizeof(arr[0]); 
+    struct TrieNode *root = getNode();
+    for(int i=0; i<n; i++)
+        insert(root,arr[i]);
+    
+    string inp;
+    cout<<"Enter the string: ";
+    cin>>inp;
+    cout<<check(root,inp)<<endl;
 }
