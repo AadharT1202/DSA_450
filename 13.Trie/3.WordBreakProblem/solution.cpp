8d5279221e8eba0aa8621b51adcc50c4cd4f4072
AadharT1202 <aadhar.toshniwal12@gmail.com>
2022-02-25 15:10:37 +0530

    Code updated

--- 13.Trie/3.WordBreakProblem/solution.cpp
+++ 13.Trie/3.WordBreakProblem/solution.cpp
@@ -1,3 +1,5 @@
+//This code passes around 10000 test cases on gfg can't find the problem.
+
 #include<iostream>
 using namespace std;
 const int Alphabet_Size = 26;
@@ -19,6 +21,7 @@ struct TrieNode* getNode()
     return temp;
 }
 
+//Inserts the strings in the structure.
 void insert(struct TrieNode *root, string key)
 {
     struct TrieNode *temp = root;
@@ -37,21 +40,19 @@ bool check(struct TrieNode *root, string inp)
     struct TrieNode* temp = root;
     if(inp.size()==0)
         return true;
+    if(inp.length()==1)
+        return root->children[inp[0]-'a']->iswordEnd;
 
     for(int i=0; i<inp.length(); i++)
     {
+        if(temp->iswordEnd==true)
+            temp = root;
+    
         int idx = inp[i]-'a';
+      
         if(temp->children[idx]==NULL)
-        {
             return false;
-        }
-        
-        else if(temp->children[idx]->iswordEnd == true){
-                temp = root;
-        }
-
-        else
-            temp = temp->children[idx];
+        temp = temp->children[idx];
 
     }
     return true;
@@ -59,10 +60,14 @@ bool check(struct TrieNode *root, string inp)
 
 int main()
 {
-    string arr[] = {"mobile","samsung","sam",
-                           "sung","ma\n","mango",
-                           "icecream","and","go","i",
-                           "like","ice","cream"};
+    string arr[] = {"b", "bbbbbbb", "baa", "abbaaa", "ababbba", "bbbbaaa", "bbaaab", "aabbaaaa", "aa", "aa", "babbb",
+     "aabbaa", "b", "bb", "aba", "bbbbbb", "aba", "bbbabaaa", "baa", "aabb", "aabb", "aaabbb", "baab", "ababa", "bb",
+     "babbbabb", "abab", "bbaaa", "ba", "aaa", "bb", "aabbba", "abab", "babaaba", "baaabab", "babba", "babbb", "ababbaa", 
+     "baab", "aababbbb", "babbba", "aaaababa", "baaabbb", "abbaaba", "bbaabaaa", "aaabbba", "abbabbaab", "abb", "abbb",
+     "baabbbaa", "babbbbbab", "b", "bbb", "bb", "b", "aababab", "abab", "b", "abb", "bbbbb", "bb", "abbb", "abbbbaa", "bbbbb",
+      "babb", "bbabaab", "abbbbaa", "bbbbbaa", "abbbab", "aababb", "ba", "bbbbaaabb", "aa", "bbbbaa","babbabbb", "bbbabbbbb",
+       "babbb", "bab", "abaaa", "bbaabb", "abbb", "abaabb", "bb", "ba", "aababbaaa", "bbbbba", "abb", "babababab", "aaabbba",
+        "abbaabb", "bbbbbabb", "aa", "bbbabbaa", "aaa", "abba", "b", "aba", "b", "bababb", "aabaaaa"};
     int n = sizeof(arr)/sizeof(arr[0]); 
     struct TrieNode *root = getNode();
     for(int i=0; i<n; i++)
