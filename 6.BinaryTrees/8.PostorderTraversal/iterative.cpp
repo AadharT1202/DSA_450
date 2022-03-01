bfb5dc3514b5bcc530f125de746e42ce4793f70a
AadharT1202 <aadhar.toshniwal12@gmail.com>
2022-03-01 17:55:23 +0530

    Postorder Iterative

---
+++ 6.BinaryTrees/8.PostorderTraversal/iterative.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+struct Node{
+    int data;
+    Node *left;
+    Node *right;
+};
+
+Node * getNode(int val)
+{
+    struct Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+
+void postorderIterative(Node* root)
+{
+    // return if the tree is empty
+    if (root == nullptr) {
+        return;
+    }
+ 
+    // create an empty stack and push the root node
+    stack<Node*> s;
+    s.push(root);
+ 
+    // create another stack to store postorder traversal
+    stack<int> out;
+ 
+    // loop till stack is empty
+    while (!s.empty())
+    {
+        // pop a node from the stack and push the data into the output stack
+        Node* curr = s.top();
+        s.pop();
+ 
+        out.push(curr->data);
+ 
+        // push the left and right child of the popped node into the stack
+        if (curr->left) {
+            s.push(curr->left);
+        }
+ 
+        if (curr->right) {
+            s.push(curr->right);
+        }
+    }
+ 
+    // print postorder traversal
+    while (!out.empty())
+    {
+        cout << out.top() << " ";
+        out.pop();
+    }
+}
+int main()
+{
+    /* Construct the following tree
+               1
+             /   \
+            /     \
+           2       3
+          /      /   \
+         /      /     \
+        4      5       6
+              / \
+             /   \
+            7     8
+    */
+ 
+    Node* root = getNode(1);
+    root->left = getNode(2);
+    root->right = getNode(3);
+    root->left->left = getNode(4);
+    root->right->left = getNode(5);
+    root->right->right = getNode(6);
+    root->right->left->left = getNode(7);
+    root->right->left->right = getNode(8);
+ 
+    postorderIterative(root);
+    cout<<endl;
+    return 0;
+}
