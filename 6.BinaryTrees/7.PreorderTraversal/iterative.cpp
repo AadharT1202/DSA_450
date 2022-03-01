c8236d2287d971a7a33de38bb79e22eb4c6f98f0
AadharT1202 <aadhar.toshniwal12@gmail.com>
2022-03-01 17:48:00 +0530

    Preorder Iterative

---
+++ 6.BinaryTrees/7.PreorderTraversal/iterative.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+struct Node{
+    int data;
+    Node *left;
+    Node *right;
+};
+
+Node * getNode(int val)
+{
+    struct Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+
+void inorderIterative(Node *root)
+{
+    stack<Node *> s;
+    s.push(root);
+    while(!s.empty())
+    {
+        Node* curr = s.top();
+        s.pop();
+        cout<<curr->data<<" ";
+
+        if(curr->right)
+            s.push(curr->right);
+        
+        if(curr->left)
+            s.push(curr->left);
+    }
+}
+int main()
+{
+    /* Construct the following tree
+               1
+             /   \
+            /     \
+           2       3
+          /      /   \
+         /      /     \
+        4      5       6
+              / \
+             /   \
+            7     8
+    */
+ 
+    Node* root = getNode(1);
+    root->left = getNode(2);
+    root->right = getNode(3);
+    root->left->left = getNode(4);
+    root->right->left = getNode(5);
+    root->right->right = getNode(6);
+    root->right->left->left = getNode(7);
+    root->right->left->right = getNode(8);
+ 
+    inorderIterative(root);
+    cout<<endl;
+    return 0;
+}
